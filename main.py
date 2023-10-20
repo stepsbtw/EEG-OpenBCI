9a0e7cda709d74249499eba77b3f10250f607e52
stepsbtw <stepsbtw@gmail.com>
2023-10-20 13:26:19 -0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas
 import matplotlib
 
-database = pandas.read_csv('DATABASES/OpenBCISession_2023-08-22_16-40-25/BrainFlow-RAW_2023-08-22_16-40-25_5')
+database = pandas.read_csv('DATABASES/data.csv')
+
+print(database.dtypes)
+print(database)
