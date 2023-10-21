ce90d900a3b2e9161a6bdcd984aba4e22449c03f
JGK19 <106597685+JGK19@users.noreply.github.com>
2023-10-21 14:57:46 -0300

    matplotlib

--- matplot_help.py
+++ matplot_help.py
@@ -1,2 +1,19 @@
 import pandas
-import matplotlib
+import matplotlib.pyplot as plt
+
+database = pandas.read_csv('DATABASES/data.csv')
+
+print(database.dtypes)
+print(database)
+
+list_time = database[' Timestamp (Formatted)'].tolist()
+list_data = database[' EXG Channel 0'].tolist()
+
+plt.subplots(nrows = 1, ncols = 1)
+plt.subplot(1, 1, 1)
+plt.step(list_time, list_data)
+plt.title('Channel 0 by Time')
+plt.ylabel('EXG Channel 0')
+plt.grid()
+
+plt.show()
