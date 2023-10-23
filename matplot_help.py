27af7300621a12c6a586c619eab538797144417f
stepsbtw <stepsbtw@gmail.com>
2023-10-23 13:31:29 -0300

    Update matplot_help.py

--- matplot_help.py
+++ matplot_help.py
@@ -1,19 +1,41 @@
 import pandas
 import matplotlib.pyplot as plt
 
-database = pandas.read_csv('DATABASES/data.csv')
+database = pandas.read_csv('DATABASES/data.txt', delimiter=', ', engine='python')
 
 print(database.dtypes)
 print(database)
 
-list_time = database[' Timestamp (Formatted)'].tolist()
-list_data = database[' EXG Channel 0'].tolist()
 
-plt.subplots(nrows = 1, ncols = 1)
-plt.subplot(1, 1, 1)
-plt.step(list_time, list_data)
-plt.title('Channel 0 by Time')
-plt.ylabel('EXG Channel 0')
-plt.grid()
+list_time = database['Timestamp (Formatted)'].tolist()
+newtime = list_time[0].split(' ') # tirando a data do tempo formatado.
+newtime = newtime[1]
+newtime = newtime.split(':') # transformando o tempo em segundos 
+newtime[0] = int(newtime[0]) * 60 * 60
+newtime[1] = int(newtime[1]) * 60
+newtime[2] = float(newtime[2])
+init = newtime[0] + newtime[1] + newtime[2]  # calculando o inicio para obter a variacao depois.
 
-plt.show()
+for i, time in enumerate(list_time): # calculo da variacao.
+    newtime = time.split(' ')
+    newtime = newtime[1]
+    newtime = newtime.split(':')
+    newtime[0] = int(newtime[0]) * 60 * 60
+    newtime[1] = int(newtime[1]) * 60
+    newtime[2] = float(newtime[2])
+    newnewtime = newtime[0] + newtime[1] + newtime[2]
+    list_time[i] = newnewtime - init
+
+plt.subplots(nrows = 2, ncols = 4)
+k=0
+list_data = []
+for i in range(4):
+    for j in range(2):
+        list_data.append(database[f'EXG Channel {k}'].tolist())
+        plt.subplot(j+1, i+1, 1)
+        plt.step(list_time, list_data[k])
+        plt.title(f'Channel {k} by Time')
+        plt.ylabel(f'EXG Channel {k}')
+        plt.grid()
+        plt.show()
+        k+=1
